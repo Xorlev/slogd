4cfcb6c40a81e7061c1316913752c7f3c6582127
Michael Rose <elementation@gmail.com>
2017-07-31 21:25:54 -0600

    storage: end to end log test
    Verifies basic properties of the log

---
+++ storage/log_test.go
@@ -0,0 +1,188 @@
+package storage_test
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/xorlev/slogd/internal"
+	pb "github.com/xorlev/slogd/proto"
+	"github.com/xorlev/slogd/storage"
+	"go.uber.org/zap"
+	"golang.org/x/net/context"
+)
+
+func TestLog_End2End(t *testing.T) {
+	tmpDir := tempdir()
+	defer os.Remove(tmpDir)
+
+	zap := logger()
+
+	log, err := storage.NewFileLog(zap, tmpDir, "end2end", &pb.TopicConfig{
+		MessageSizeLimit:          4 * 1024 * 1024,
+		SegmentSizeLimit:          4 * 1024, // force new segments every few messages
+		IndexAfterBytes:           256,
+		RotateSegmentAfterSeconds: 24 * 3600,
+		StaleSegmentSeconds:       720 * 3600,
+		LogMaintenancePeriod:      300,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	go func(ch storage.LogEntryChannel) {
+		for {
+			_, ok := <-ch
+			if !ok {
+				break
+			}
+		}
+	}(log.LogChannel())
+
+	// Produce messages
+	for i := uint64(0); i < 1000; i++ {
+		ctx := context.WithValue(context.Background(), "requestStart", time.Now())
+		offset, err := log.Append(ctx, []*pb.LogEntry{
+			&pb.LogEntry{
+				Entry: &pb.LogEntry_RawBytes{
+					RawBytes: []byte("helloworld"),
+				},
+			},
+		})
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(offset) > 1 {
+			t.Fatal("More than 1 offset returned, expected 1")
+		}
+
+		if offset[0] != i {
+			t.Fatalf("Expected offset %d, Append returned: %d", i, offset[0])
+		}
+	}
+
+	// Consume them again one at a time
+	for i := uint64(0); i < 1000; i++ {
+		q := &storage.LogQuery{
+			StartOffset: i,
+			MaxMessages: 1,
+		}
+
+		retrieval, err := log.Retrieve(context.Background(), q, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(retrieval.Logs) != 1 {
+			t.Fatalf("Expected 1 log, found: %d", len(retrieval.Logs))
+		}
+
+		if retrieval.Logs[0].Offset != i {
+			t.Fatalf("Expected log at offset %d, actual: %+v", i, retrieval.Logs[0])
+		}
+	}
+
+	// Consume them all at once
+	{
+		q := &storage.LogQuery{
+			StartOffset: 0,
+			MaxMessages: 1000,
+		}
+
+		retrieval, err := log.Retrieve(context.Background(), q, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(retrieval.Logs) != 1000 {
+			t.Fatalf("Expected 1000 log, found: %d", len(retrieval.Logs))
+		}
+
+		for i, log := range retrieval.Logs {
+			if log.Offset != uint64(i) {
+				t.Fatalf("Expected log at offset %d, actual: %+v", i, log)
+			}
+		}
+	}
+
+	// Consume them 50 at a time via cursor
+	{
+		logsPerBatch := uint32(50)
+
+		q := &storage.LogQuery{
+			StartOffset: 0,
+			MaxMessages: logsPerBatch,
+		}
+
+		ch := make(storage.LogEntryChannel, 1)
+
+		// log.Retrieve(context.Background(), q, continuation)
+		cursor := storage.NewCursor(context.Background(), q, ch, logsPerBatch)
+
+		zap.Sync()
+
+		consumed := make(storage.LogEntryChannel, 1000)
+		err := cursor.Consume(log, func(resp interface{}) error {
+			response := resp.(*pb.GetLogsResponse)
+
+			zap.Sync()
+
+			if len(response.Logs) != 1 {
+				t.Fatalf("Expected 1 log, had: %d", len(response.Logs))
+			}
+
+			for _, l := range response.Logs {
+				consumed <- &internal.TopicAndLog{
+					LogEntry: l,
+					Topic:    "end2end",
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		i := uint64(0)
+		for entry := range consumed {
+			if entry.LogEntry.Offset != i {
+				t.Fatalf("Expected offset %d, found %+v", i, entry)
+			}
+			i += 1
+
+			if i == 1000 {
+				break
+			}
+		}
+
+		close(consumed)
+		close(ch)
+	}
+
+	log.Close()
+}
+
+func logger() *zap.SugaredLogger {
+	cfg := zap.NewDevelopmentConfig()
+	logger, _ := cfg.Build()
+	defer logger.Sync() // flushes buffer, if any
+
+	return logger.Sugar()
+}
+
+func tempdir() string {
+	f, err := ioutil.TempDir("", "slogd-")
+	if err != nil {
+		panic(err)
+	}
+	// if err := os.Mkdir(f, 0666); err != nil {
+	// 	panic(err)
+	// }
+	// if err := os.Remove(f); err != nil {
+	// 	panic(err)
+	// }
+	return f
+}
